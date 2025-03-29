9900d3525d35b7b47010f09cefb355ef11d4d74a
jirofg <mechcromancer@gmail.com>
2025-03-28 20:45:55 -0600

    [FEAT] views for list, create, update and delete courses and modules

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
+    path('accounts/login/', LoginView.as_view(), name='login'),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
+    path('courses/', include('courses.urls')),
     path('admin/', admin.site.urls),
 ]
 
