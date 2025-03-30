f18cbb8374d91f31c3ea8f5d5200de1c3fbaabe0
jirofg <mechcromancer@gmail.com>
2025-03-29 18:09:40 -0600

    [FEAT] View for update module's content

--- courses/urls.py
+++ courses/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ManageCourseListView, CourseCreateView, CourseUpdateView, CourseDeleteView, CourseModuleUpdateView, ContentCreateUpdateView, ContentDeleteView
+from .views import ManageCourseListView, CourseCreateView, CourseUpdateView, CourseDeleteView, CourseModuleUpdateView, ContentCreateUpdateView, ContentDeleteView, ModuleContentListView, ModuleOrderView, ContentOrderView
 
 app_name='courses'
 
@@ -12,4 +12,8 @@ urlpatterns = [
     path('module/<int:module_id>/content/<model_name>/create/', ContentCreateUpdateView.as_view(), name='module_content_create'),
     path('module/<int:module_id>/content/<model_name>/<id>/', ContentCreateUpdateView.as_view(), name='module_content_update'),
     path('content/<int:id>/delete/', ContentDeleteView.as_view(), name='module_content_delete'),
+    path('module/<int:module_id>/', ModuleContentListView.as_view(), name='module_content_list'),
+    # api
+    path('module/order/', ModuleOrderView.as_view(), name='module_order'),
+    path('content/order/', ContentOrderView.as_view(), name='content_order')
 ]
